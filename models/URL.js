06874441bb163101c2def2164ff47bd99cfb7b9a
pleasesailas <pleasesailas@gmail.com>
2023-04-07 09:37:43 +0800

    feat:add bootstrap & modify main.handlebars

---
+++ models/URL.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlSchema = new Schema({
+  originURL: {
+    type: String,
+    require: true
+  },
+  shortURL: {
+    type: String,
+    require: true
+  }
+}) 
+
+module.exports = mongoose.model('URL', urlSchema)
