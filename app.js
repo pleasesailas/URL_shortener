276aa985b291cf6263a3f7718353a2cf77637c81
pleasesailas <pleasesailas@gmail.com>
2023-04-07 08:51:50 +0800

    feat: setting mongoose connected

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
+//mongoose
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 //views
-app.engine('handlebars', exphbs({defaultLayout:'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-
+//routes
 app.get('/', (req, res) => {
   res.render('index')
 })
